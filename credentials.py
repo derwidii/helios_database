e59f92b8bd291ddf040807b4c4c76276f54b547b
derwidii <121177245+derwidii@users.noreply.github.com>
2024-03-16 11:03:07 +0100

    Add files via upload

---
+++ credentials.py
@@ -0,0 +1,7 @@
+# credentials.py
+connection_config = {
+    "host": "5.75.158.75",
+    "user": "read-only-user",
+    "password": "aris-read-only",
+    "database": "dacs_temp_copy_do_not_use_after_march_2024",
+}

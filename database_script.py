9fe5e14f137553e22b56319e1249d2aefef5a957
derwidii <121177245+derwidii@users.noreply.github.com>
2024-03-17 12:40:49 +0100

    Update database_script.py

--- database_script.py
+++ database_script.py
@@ -9,7 +9,7 @@ import os
 connection_config = {
         "host": st.secrets["DB_HOST"],
         "user": st.secrets["DB_USER"],
-        "password": st.secrets["DB_PASSWORD"]),
+        "password": st.secrets["DB_PASSWORD"],
         "database": st.secrets["DB_NAME"],
     }
 
